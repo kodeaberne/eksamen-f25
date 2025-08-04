20f670ed84041f551455b89cb8084076a4b671bb
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-08-04 22:10:54 +0200

    feat(fonts): add fonts
    Added fonts via Astros experimental fonts API and added them to
    tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +7,39 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Inclusive Sans',
+        weights: [400, 500, 600, 700],
+        cssVariable: '--font-body'
+      },
+      {
+        provider: "local",
+        name: "RasterForgeRegular",
+        cssVariable: "--font-heading",
+        variants: [
+          {
+            src: [
+              "./src/assets/RasterForgeRegular.woff2"
+            ]
+            }
+        ]
+      },
+      {
+        provider: "local",
+        name: "Gill Sans Italic",
+        cssVariable: "--font-pill",
+        variants: [
+          {
+            src: [
+              "./src/assets/GillSansItalic.otf"
+            ]
+          }
+        ]
+      }
+    ]
   }
 });
