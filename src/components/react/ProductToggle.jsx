c58eee592048e7ddfd5e9ec74e877ad710332ca6
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-08-05 22:31:15 +0200

    fix(react): fix react class name bugs
    Fixed two instances of class to className.

--- src/components/react/ProductToggle.jsx
+++ src/components/react/ProductToggle.jsx
@@ -1,7 +1,7 @@
 export default function ProductToggle() {
     return (
         <button className="flex flex-row gap-0 | items-center | cursor-pointer">
-            <h2 class="font-heading text-text-menu-active text-2xl">Produkter</h2>
+            <h2 className="font-heading text-text-menu-active text-2xl">Produkter</h2>
             <img src="images/chevronDown.svg"></img>
         </button>
     )
