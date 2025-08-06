eed43044d21c26b149d40f35caab7a23b531f0c0
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-08-06 21:22:33 +0200

    feat(ProductToggle): add toggle functionality
    Added toggle functionality to the button, and fixed minor react bugs.

--- src/components/react/CatButton.jsx
+++ src/components/react/CatButton.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+
 export default function CatButton(name) {
+
     return (
         <button className="flex-row gap-0 items-center cursor-pointer hidden lg:flex">
         <h2 className="font-heading text-text-menu-active text-2xl" name={name}>{ name.name }</h2>
-        <img src="images/chevronDown.svg"></img>
+        <img src="/images/chevronDown.svg" alt="Dropdown arrow"></img>
     </button>
     )
 }
