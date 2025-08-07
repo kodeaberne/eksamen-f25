d2c0c497361c58d517afec253b9a58c86d9cd590
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-08-07 21:52:54 +0200

    feat(color): add hover color
    Added hover color to navbar text buttons.

--- src/components/react/CatButton.jsx
+++ src/components/react/CatButton.jsx
@@ -4,7 +4,7 @@ export default function CatButton(name) {
 
     return (
         <button className="flex-row gap-0 items-center cursor-pointer hidden lg:flex">
-        <h2 className="font-heading text-text-menu-active text-2xl" name={name}>{ name.name }</h2>
+        <h2 className="font-heading text-text-menu-active text-2xl hover:text-text-menu-hover" name={name}>{ name.name }</h2>
         <img src="/images/chevronDown.svg" alt="Dropdown arrow"></img>
     </button>
     )
